5a1fe1cb7976726edbce5f753fd591654039cbc3
halexandru11 <77728536+halexandru11@users.noreply.github.com>
2026-03-09 11:26:13 +0200

    add useful comments for the production plan problem

--- h2/production-plan.py
+++ h2/production-plan.py
@@ -2,21 +2,21 @@ from ortools.sat.python import cp_model
 
 
 def main():
-    max_gas = 50
-    max_chloride = 40
+    max_gas = 50  # Nitrogen is the limit
+    max_chloride = 40  # Chlorine is the limit
 
     model = cp_model.CpModel()
 
     gas = model.new_int_var(0, max_gas, "gas")
     chloride = model.new_int_var(0, max_chloride, "chloride")
 
-    model.add(gas + chloride <= 50)
-    model.add(3 * gas + 4 * chloride <= 180)
-    model.add(chloride <= 40)
+    model.add(gas + chloride <= 50)  # Nitrogen is the limit
+    model.add(3 * gas + 4 * chloride <= 180)  # Hydrogen is the limit
+    model.add(chloride <= 40)  # Chlorine is the limit
 
     objective = [
-        cp_model.LinearExpr.term(gas, 40),
-        cp_model.LinearExpr.term(chloride, 50),
+        cp_model.LinearExpr.term(gas, 40),  # Gas profits 40€
+        cp_model.LinearExpr.term(chloride, 50),  # Chloride profits 50€
     ]
     model.maximize(cp_model.LinearExpr.sum(objective))
 
